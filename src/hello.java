5685614d3e8e69f09a77a488802fd91a76a5e56f
tahbo763 <tahbo763@student.liu.se>
2011-10-05 11:26:31 +0200

    adding hello class
    Signed-off-by: tahbo763 <tahbo763@student.liu.se>

---
+++ src/hello.java
@@ -0,0 +1,13 @@
+
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		System.out.println("First edition for learning Git");
+
+	}
+
+}
